06d099b72abfdbc865322e187c3b434a15363b08
ŽAN BIZJAK <zb0382@student.uni-lj.si>
2018-11-20 22:00:02 +0100

    Zbrisal Shared mapo z importanim html-jem. S tem se je odstranila orodna vrstica in vse ostalo iz templata

--- HouCell/HouCell/Startup.cs
+++ HouCell/HouCell/Startup.cs
@@ -56,7 +56,7 @@ namespace HouCell
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Login}/{action=Index}/{id?}");
             });
         }
     }
