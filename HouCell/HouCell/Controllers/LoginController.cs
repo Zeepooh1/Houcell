4164308aeb7629eb1501f83f987ce2a40d13ded3
ŽAN BIZJAK <zb0382@student.uni-lj.si>
2018-11-23 10:45:48 +0100

    povezava na mysql bazo. Spremeni connectionstring za uspešno povezavo

--- HouCell/HouCell/Controllers/LoginController.cs
+++ HouCell/HouCell/Controllers/LoginController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -13,6 +14,25 @@ namespace HouCell.Controllers
         // GET: /<controller>/
         public IActionResult Index()
         {
+
+            using (var connection = new MySqlConnection
+            {
+                ConnectionString = "server=localhost;user id=root;password=;persistsecurityinfo=True;port=3306;database=Houcell"
+            })
+            {
+                connection.Open();
+                var command = new MySqlCommand("SELECT * FROM uporabnik;", connection);
+
+                using (MySqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        ViewData["userID"] = reader["userID"] + "";
+                        ViewData["userName"] = reader["userName"];
+                        ViewData["password"] = reader["password"];
+                    }
+                }
+            }
 
             return View();
         }
