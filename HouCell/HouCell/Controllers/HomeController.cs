db2e9a5d914e5aa6eb2466777aadd97d5f67e892
ŽAN BIZJAK <zb0382@student.uni-lj.si>
2018-11-24 17:54:14 +0100

    Izpis senzorjev po hišah

--- HouCell/HouCell/Controllers/HomeController.cs
+++ HouCell/HouCell/Controllers/HomeController.cs
@@ -19,9 +19,58 @@ namespace HouCell.Controllers
             {
                 return RedirectToAction("Index", "Login");
             }
-            var entities = new HoucellContext();
-            
-            return View(entities.Senzorji.ToList());
+            // uID je  userID od prijavljenga uporabnika. Ga shranim za lažjo uporabo kasneje
+            int uID = (int)HttpContext.Session.GetInt32("logID");
+
+            //nov HoucellContext (pravino bi blo z dependency injectionom ampak mi ni jasno kako haha)
+            var context = new HoucellContext();
+
+            //inicializacija novega HiseModel modela
+            HiseModel hiseModel = new HiseModel();
+
+            /*
+             * HiseSeznam je atribut od modela (IList) in vanj shranimo vse hiše
+             * ki jih prijavljeni uporabnik ima v lasti.
+             * 
+             * identični SQL querry bi bil:
+             * SELECT * FROM hisa h, uporabnik u 
+             * 
+             * INNER JOIN soba s ON s.hisaID = h.hisaID
+             * INNER JOIN senzorji sens ON sens.sobaID = s.sobaID
+             * WHERE u.userID =  h.userID;
+             */ 
+             hiseModel.HiseSeznam = (from hisa in context.Hisa
+                         join sobe in context.Soba
+                         on hisa.HisaId equals sobe.HisaId
+                         join senzorji in context.Senzorji
+                         on sobe.SobaId equals senzorji.SobaId
+                         where hisa.UserId == uID
+                         select new Hisa()
+                         {
+                             HisaId = hisa.HisaId,
+                             UserId = hisa.UserId,
+                             Naslov = hisa.Naslov,
+                             Soba = hisa.Soba,
+                             User = hisa.User
+                             
+                         }).ToList();
+
+            /*
+             * hiseModel.Senzorji je IList iz modela hiseModel v katerega
+             * shranim vse senzorje v bazi (lahko bi bolje naredil, sam se mi neda haha)
+             */ 
+            hiseModel.Senzorji = (from senzor in context.Senzorji
+                                  select new Senzorji()
+                                  {
+                                      SenzorId = senzor.SenzorId,
+                                      SobaId = senzor.SobaId,
+                                      VrednostSenzorja = senzor.VrednostSenzorja,
+                                      Senzor = senzor.Senzor,
+                                      Soba = senzor.Soba
+                                  }).ToList();
+
+            //v View(Index.cshtml) pošljem hiseModel
+            return View(hiseModel);
         }
 
 
---
+++ HouCell/HouCell/Models/HiseModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HouCell.Models
+{
+    /************************************************************************************************************************************************************************
+     * Model HiseModel                                                                                                                                                      *
+     * naredu sm 2 ILista: HiseSeznam v katerga v HomeController dam use hiše od vpisanga uporabnika                                                                        *
+     *                     Senzorji v katerga grejo vsi senzorji iz baze. Lahko bi dau samo senzorje od uporabnika sam se mi ni dal in to preverjam kasnej v Index Viewu    *
+     ************************************************************************************************************************************************************************/
+    public partial class HiseModel
+    {
+        public IList<Hisa> HiseSeznam { get; set; }
+        public IList<Senzorji> Senzorji { get; set; }
+    }
+}
