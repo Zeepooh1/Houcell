b5e1a271a9fe5823f6ba4cd64146e7cf346d5b84
PLOHL, JAŠA <jp0511@student.uni-lj.si>
2019-01-06 19:36:20 +0100

    nc nouga, razn tist, kar si mi ti narekovou haha

---
+++ HouCell/HouCell/Controllers/ApiController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HouCell.RepositoryInterfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HouCell.Controllers
+{
+    [Route("api/login")]
+    [ApiController]
+    public class ApiController : Controller
+    {
+        ILoggerManager _logger;
+        IRepositoryWrapper _wrapper;
+        public ApiController(ILoggerManager logger, IRepositoryWrapper wrapper)
+        {
+            _logger = logger;
+            _wrapper = wrapper;
+        }
+
+        [HttpPost]
+        public IActionResult Index(string userName, string pass)
+        {
+
+            //bool success = false;
+
+            int logID = _wrapper.Uporabnik.UporabnikExists(userName, pass);
+
+            return Ok(logID);
+
+        }
+
+    }
+}
