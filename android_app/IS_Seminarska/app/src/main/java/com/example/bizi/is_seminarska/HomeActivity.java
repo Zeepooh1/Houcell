b3fd5516b292c2ad6041865449541022ed4305a2
ŽanBizjak <zb0382@student.uni-lj.si>
2019-01-08 18:30:46 +0100

    začel delati Dynamic layout

--- android_app/IS_Seminarska/app/src/main/java/com/example/bizi/is_seminarska/HomeActivity.java
+++ android_app/IS_Seminarska/app/src/main/java/com/example/bizi/is_seminarska/HomeActivity.java
@@ -2,12 +2,24 @@ package com.example.bizi.is_seminarska;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
+
+    LinearLayout linearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        linearLayout = findViewById(R.id.linear_layout);
+
+        for(int i = 0; i < 3; i++){
+            TextView tv = new TextView(this);
+            tv.setText("TEST" + String.valueOf(i));
+            linearLayout.addView(tv);
+        }
     }
 }
